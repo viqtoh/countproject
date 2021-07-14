f65f4c55cec5c373e9ea4d795bc074d9c2e1ed31
Victor <victoraromiwe@yahoo.com>
2021-07-14 14:36:54 +0100

    creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var count = 0;
+var range = 1;
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      count : 0,
+      range : 1,
+    };
+    this.inc = this.inc.bind(this);
+    this.rangeChange = this.rangeChange.bind(this);
+  }
+  inc(val){
+    if (val == 2){
+      count = count +range;
+      this.setState({count : count});
+    }
+    else if(val == 3){
+      count =count -range;
+      this.setState({count : count});
+    }
+    return {__html: count};
+  }
+  rangeChange(){
+    var value = document.getElementById('field').value;
+    if (parseInt(value)){
+      range =parseInt(value);
+      this.setState({range : value});
+    }
+    else{
+      document.getElementById('field').placeholder='Invalid number...';
+      document.getElementById('field').value='';
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header"><p></p>
+          <p>Range: {range}</p>
+          <p dangerouslySetInnerHTML = {this.inc(0)}></p>
+          <div className="butDiv">
+          <button
+            className="Inc"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => this.inc(2)}
+          >
+            Increment++
+          </button>
+          <button
+            className="Dec"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => this.inc(3)}
+          >
+            Decrement--
+          </button><br></br>
+          <input className='field' onKeyUp={() => this.rangeChange()} id='field' type='text' placeholder='Range to count...'></input>
+          </div>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
